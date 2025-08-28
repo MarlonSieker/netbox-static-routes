a668b4676b5ed3199c673503a7f4605917fea4b0
Marlon Sieker <Marlon.Sieker@tkrz.de>
2025-08-28 15:59:24 +0200

    v1.2.0

--- netbox_static_routes/__init__.py
+++ netbox_static_routes/__init__.py
@@ -4,7 +4,7 @@ class NetboxStaticRoutesConfig(PluginConfig):
     name = 'netbox_static_routes'
     verbose_name = 'Netbox Static Routes'
     description = 'Display static routes in Netbox'
-    version = '1.1.0'
+    version = '1.2.0'
     min_version = '4.3.0'
     base_url = 'static-routes'
     author = 'Marlon Sieker'
