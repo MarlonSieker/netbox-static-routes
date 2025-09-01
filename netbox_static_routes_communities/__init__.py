9dc4c06f3af82d80fff41d82f9e7f148f939b781
Marlon Sieker <Marlon.Sieker@tkrz.de>
2025-09-01 11:38:09 +0200

    Added initial migration

--- netbox_static_routes_communities/__init__.py
+++ netbox_static_routes_communities/__init__.py
@@ -1,7 +1,7 @@
 from netbox.plugins import PluginConfig
 
 class NetboxStaticRoutesConfig(PluginConfig):
-    name = 'netbox_static_routes'
+    name = 'netbox_static_routes_communities'
     verbose_name = 'Netbox Static Routes'
     description = 'Display static routes in Netbox'
     version = '1.2.0'
